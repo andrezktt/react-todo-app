9d301fc484591bad511630c21a6fc9853343fa06
andre_zktt <andrezicatti@gmail.com>
2024-07-12 18:30:20 -0300

    12072024

---
+++ backend/src/config/cors.js
@@ -0,0 +1,6 @@
+module.exports = function(req, res, next) {
+   res.header('Access-Control-Allow-origin', '*')
+   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PATCH, DELETE')
+   res.header('Access-Control-Allow-Headers', 'Origins, X-Requested-With, Content-Type, Accept')
+   next()
+}
