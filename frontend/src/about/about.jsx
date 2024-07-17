b9fb10c1a5a710da4ee123bbf6f2d0b8b08860c8
andre_zktt <andrezicatti@gmail.com>
2024-07-17 18:43:43 -0300

    17072024

---
+++ frontend/src/about/about.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+import PageHeader from "../template/pageHeader"
+
+export default props => (
+   <div>
+      <PageHeader name='Sobre' small='Nós' />
+
+      <h2>Nossa História</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi sapiente praesentium maxime explicabo, assumenda nihil nemo doloribus eius! Aliquid ratione maxime placeat sequi quis fugiat a ex fuga id alias.</p>
+
+      <h2>Nossa Missão</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe, nisi dolorem. Deserunt sed, quo dolorem, rerum aliquam magni ipsum consequatur neque ipsam quibusdam voluptatum necessitatibus delectus recusandae non saepe impedit!</p>
+
+      <h2>Imprensa</h2>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quaerat nisi culpa repudiandae deserunt sequi voluptate explicabo tenetur excepturi expedita, asperiores obcaecati esse officia ea adipisci doloribus cum, voluptas neque debitis.</p>
+   </div>
+)
---
+++ frontend/src/main/routes.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Router, Route, Redirect, hashHistory } from 'react-router'
+
+import ToDo from '../todo/todo'
+import About from '../about/about'
+
+export default props => (
+   <Router history={hashHistory}>
+      <Route path='/todos' component={ToDo} />
+      <Route path='/about' component={About} />
+      <Redirect from='*' to='/todos' />
+   </Router>
+)
+
